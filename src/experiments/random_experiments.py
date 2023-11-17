bd3f0a0b1fd2d701ce8efd300d3936d1dfea1a8c
Blaise143 <blaiseappo@gmail.com>
2023-11-16 23:57:47 -0800

    added experiments

--- src/experiments/random_experiments.py
+++ src/experiments/random_experiments.py
@@ -6,9 +6,14 @@ import numpy as np
 
 # Checking image dimensions
 parent_dir = os.getcwd()
-image_path = parent_dir+"/llama-duck-ds/train/duck/00AONW8ADLQD.jpg"
-pil_image = Image.open(image_path)
-image = np.array(pil_image)
+# image_path = parent_dir+"/llama-duck-ds/train/duck/00AONW8ADLQD.jpg"
+# pil_image = Image.open(image_path)
+# image = np.array(pil_image)
+att = nn.MultiheadAttention(embed_dim=8, num_heads=8)
 
+# pose_embed = nn.Embedding(n)
 if __name__ == "__main__":
-    print(image.shape)
+    # print(image.shape)
+    rand = torch.ones(10, 8)
+    out, _ = att(rand, rand, rand)
+    print(out.shape)
