0f47aee6bbfee26e67d1edc305c34c3f8571de19
Blaise143 <blaiseappo@gmail.com>
2023-11-16 21:03:09 -0800

    Added Multiheaded attention

--- src/SEViT/Attention.py
+++ src/SEViT/Attention.py
@@ -15,30 +15,14 @@ class SelfAttention(nn.Module):
     def __init__(self, dim: int) -> None:
         """
         Takes in a tensor and spits out a tensor of the same dimension after undergoing self attention
-
+        NOTE: dim is the dimension of the embedding.
         Args:
-            input_dim (torch.Tensor): The input dimension of the embedding
+            dim (torch.Tensor): The input dimension of the embedding
         """
         super(SelfAttention, self).__init__()
         self.dim = dim
-        self.Q = nn.Linear(dim, dim)
-        self.K = nn.Linear(dim, dim)
-        self.V = nn.Linear(dim, dim)
 
-    def forward(self, X: torch.Tensor) -> torch.Tensor:
-        """Performs self attention on input tensor X
-
-        Args:
-            X (tensor): the input
-
-        Returns:
-            Tensor: an output after self attention is acted on it
-        """
-
-        Q = self.Q(X)
-        K = self.K(X)
-        V = self.V(X)
-
+    def forward(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor):
         scores = torch.matmul(Q, K.T)
         scores_normalized = scores / np.sqrt(self.dim)
         softed = nn.Softmax(dim=-1)(scores_normalized)
@@ -59,6 +43,14 @@ class MultiHeadedAttention(nn.Module):
         super().__init__()
         self.dim = dim
         self.num_heads = num_heads
+        self.head_dim = dim // num_heads
+
+        assert self.head_dim * self.num_heads == dim, "The dimensions must be divisible by num_heads"
+
+        self.query_layers = nn.ModuleList([nn.Linear(dim, self.head_dim) for _ in range(num_heads)])
+        self.key_layers = nn.ModuleList([nn.Linear(dim, self.head_dim) for _ in range(num_heads)])
+        self.value_layers = nn.ModuleList([nn.Linear(dim, self.head_dim) for _ in range(num_heads)])
+        self.fc = nn.Linear(in_features=dim, out_features=dim)
 
     def forward(self, X: torch.tensor) -> torch.tensor:
         """Performs a forward pass throught the network
@@ -69,19 +61,23 @@ class MultiHeadedAttention(nn.Module):
         Returns:
             torch.tensor: The output tensor after a multiheaded attention operation
         """
-        acc = list()
+        attention_heads = []
+        for i in range(self.num_heads):
+            queries = self.query_layers[i](X)
+            keys = self.key_layers[i](X)
+            values = self.value_layers[i](X)
 
-        for _ in range(self.num_heads):
-            acc.append(SelfAttention(dim=self.dim)(X))
-        attentions = torch.cat(acc, -1)
-        W = nn.Linear(in_features=attentions.shape[1],
-                      out_features=self.dim)
-        out = W(attentions)
-        return out
+            attention = SelfAttention(self.head_dim)(queries, keys, values)
+            attention_heads.append(attention)
+
+        print(attention_heads)
+        attention_heads = torch.cat(attention_heads, dim=-1)
+        output = self.fc(attention_heads)
+        return output
 
 
 if __name__ == "__main__":
-    random_tensor = torch.rand(9, 9)
-    # print(MaskedAttention(9)(random_tensor)
-    print(SelfAttention(9)(random_tensor).shape)
-    # print(MultiHeadedAttention(5)(random_tensor).shape)
+    att = MultiHeadedAttention(dim=16, num_heads=8)
+    tensor = torch.ones(10, 16)
+    out = att(tensor)
+    print(out.shape)
