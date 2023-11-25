f290332fbfd8e68873a0165916592239c0a3831c
Alex <al20vortex@gmail.com>
2023-11-24 17:56:15 -0800

    Completed some of the todos

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import torch
 import torchvision
-import torch
 from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-
 # Define our hyperparameters
 INPUT_SHAPE = (64, 64)
 OUTPUT_SHAPE = (1,)
-POSSIBLE_MODULES = [nn.Conv2d, ]
+POSSIBLE_MODULES = [nn.Conv2d, nn.MaxPool2d]  # Added MaxPool2d for diversity
 
-# TODO set up these hyper params as config options for each possible module
+# Set up hyperparameters for each possible module
 module_config = {
     "Conv2d": {
-        "kernel_size": [1, 3, 5],
-        "padding": [0, 1, 2],
-        "activation": [nn.ReLU, nn.LeakyReLU]
+        "in_channels": [3, 64, 128],  # Example channel sizes
+        "out_channels": [64, 128, 256],  # Adjust as per your requirement
+        "kernel_size": [3, 5],
+        "stride": [1, 2],
+        "padding": [0, 1]
     },
+    "MaxPool2d": {
+        "kernel_size": [2, 3],
+        "stride": [2],
+        "padding": [0, 1]
+    }
 }
 
 def get_device():
@@ -28,46 +33,37 @@ def get_device():
     else:
         return torch.device("cpu")
 
-# Use the function to set the device
-device = get_device()
-print(f"Using device: {device}")
+device =  get_device()
 
 class NeuralNet(nn.Module):
-    def __init__(self):		
+    def __init__(self):      
         super(NeuralNet, self).__init__()
-        
+
         # input layer
-        self.input_layer = nn.Sequential(nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1), nn.ReLU())
+        self.input_layer = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1), nn.ReLU())
         self.layers = nn.ModuleList()
 
-        # Currently using a preset output layer which lowers the resolution from 64x64 down to 1x1
-        #  TODO This needs to change in the future to be more adaptive/nonrigid
-        self.out = nn.Sequential(
-            nn.Conv2d(3, 1, kernel_size=3, stride=1, padding=1),   # TODO we need to make this infer somehow so that it connects to how many feature maps were in the previous layer. 
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(1*64*64, 1),
-            nn.Sigmoid()
-        )
+        # output layer placeholder
+        self.out = None
+        self.adapt_output_layer(64, 64, 64)
 
     def adapt_output_layer(self, num_feature_maps, x_dim, y_dim):
         self.out = nn.Sequential(
             nn.ReLU(),
             nn.Flatten(),
-            nn.Linear(num_feature_maps*x_dim*y_dim, OUTPUT_SHAPE),
+            nn.Linear(num_feature_maps * x_dim * y_dim, OUTPUT_SHAPE[0]),
             nn.Sigmoid()
         )
 
-    def add_layer(self, module: nn.Module):
-        self.layers.append(module())
-    
+    def add_layer(self, module: nn.Module, **kwargs):
+        self.layers.append(module(**kwargs))
+
     def forward(self, x):
         x = self.input_layer(x)
         for layer in self.layers:
             x = layer(x)
         x = self.out(x)
-        x = x.view(x.size(0))
-        return x
+        return x.view(x.size(0))
 
 # TODO params needs to be useful information based off we decide to expand. For now we will simply expand every second epoch
 def need_to_expand(params, epoch):
@@ -86,7 +82,6 @@ def get_module_benefit(params, module):
     # stub
     return 0.5                    
 
-
 # Define transformations for the dataset
 transform = transforms.Compose([
     transforms.Resize(INPUT_SHAPE),  # Resize images
@@ -102,16 +97,13 @@ val_dataset = datasets.ImageFolder('llama-duck-ds/val', transform=transform)
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
-
 def train():
-
-    # TODO complete the code to load the dataset
     model = NeuralNet().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     loss_fun = nn.BCELoss()
 
     for epoch in range(1000):
-        for input, target in train_loader:  # Iterate over your data
+        for input, target in train_loader:  
             input, target = input.to(device).float(), target.to(device).float()
             optimizer.zero_grad()
             output = model(input)
@@ -119,29 +111,24 @@ def train():
             loss.backward()
             optimizer.step()
 
-        print(f'Epoch: {epoch} Loss: {loss}')
-        
-        # perform some mechanism to determine whether or not to add 
-        # check if we need to add a module/layer/neuron
+        print(f'Epoch: {epoch} Loss: {loss.item()}')
+
         if need_to_expand({}, epoch):
-            # see where we're adding the layer in our model
-            for i, layer in enumerate(model.layers):
-                # crappy name, rename to something else 
-                benefit_scores = torch.zeros(len(POSSIBLE_MODULES))
 
-                for j, module in enumerate(POSSIBLE_MODULES):
-                    # TODO in here we should keep track of the shape of the last layer before the output.
-                    #   This is in order to ensure the layer we're adding can accept inputs of this shape
+            # Same as the forward function but we get the last thing in model layers 
+            # Determine the shape of the last layer
+            x = torch.rand((1, 3, 64, 64)).to(device).float()  # Dummy input for shape inference
+            x = model.input_layer(x)
+            for layer in model.layers:
+                x = layer(x)
+            shape = x.shape  # Shape before the output layer
 
-                    # also for each laeyr, we should have a list of possible initialization options
+            # TODO: Define logic to choose best module and its configuration
+            # As an example, adding a Conv2d layer
+            best_module = nn.Conv2d
+            best_module_config = {"in_channels": shape[1], "out_channels": 128, "kernel_size": 3, "stride": 1, "padding": 1}
 
-                    # check the benefit score of adding this module
-                    benefit_scores[j] = get_module_benefit({}, module)
-                
-                best_module = POSSIBLE_MODULES[torch.argmax(benefit_scores)]
-
-                # work out the parameters for that module
-                # like if its a conv layer we need to figure out the dims for that spot
-                # model.add_layer(best_module) TODO DOESNT WORK
+            model.add_layer(best_module, **best_module_config)
+            model.adapt_output_layer(128, shape[2], shape[3])  # Update output layer
 
 train()
