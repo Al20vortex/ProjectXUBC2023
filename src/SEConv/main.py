36876f3b12d4e11d423ecb1cde555ed94c0e698f
Blaise143 <blaiseappol@gmail.com>
2023-12-29 12:08:28 -0800

    a few more stuff to be done

--- src/SEConv/main.py
+++ src/SEConv/main.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from networks import *
-from networks.utils import emnist_train, emnist_val
+from networks.utils import emnist_train, emnist_val, cifar_test, cifar_train
 from networks.utils import train, get_device
 import copy
+import numpy as np
+import matplotlib.pyplot as plt
 
 device = get_device()
 
@@ -14,6 +16,11 @@ print(emnist_train[0][0].shape)
 train_loader = DataLoader(emnist_train, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(emnist_val, batch_size=BATCH_SIZE, shuffle=False)
 
+cifar_train_loader = DataLoader(
+    cifar_train, batch_size=BATCH_SIZE, shuffle=True)
+cifar_test_loader = DataLoader(
+    cifar_test, batch_size=BATCH_SIZE, shuffle=False)
+
 channels_list = [1, 8, 8]
 n_classes = 10
 model = DynamicCNN(channels_list=channels_list, n_classes=10)
@@ -21,16 +28,30 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
-if __name__ == "__main__":
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize if necessary
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
 
-    model = DynamicCNN(channels_list, 10, 0.2)
-    history = train(
-        model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        expansion_threshold=2,
-        epochs=EPOCHS
-    )
+
+if __name__ == "__main__":
+    images, labels = next(iter(cifar_test_loader))
+    print(f"cifar_shape: {images[0].shape}")
+    imshow(images[0])
+    # print(cifar_test[0])
+    # print(cifar_test_loader)
+
+    # ___
+    # model = DynamicCNN(channels_list, 10, 0.2)
+    # history = train(
+    #     model=model,
+    #     train_loader=cifar_train_loader,
+    #     val_loader=cifar_test_loader,
+    #     expansion_threshold=1.5,
+    #     epochs=EPOCHS
+    # )
+    # ___
     # print("****")
     # print(model.convs)
     # print("***")
